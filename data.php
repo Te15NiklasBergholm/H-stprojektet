16e24ca61bf73bbdf67b50cc98966497560676bf
jensnti <jens.andreasson@nti.se>
2017-09-19 09:39:55 +0200

    first commit

---
+++ data.php
@@ -0,0 +1,17 @@
+<?php
+
+$data = array(
+array(
+	"author" => "Jens",
+	"title" => "Lorem",
+	"message" => "Lorem ipsum dolor sit amet..."),
+array(
+	"author" => "Jens",
+	"title" => "Lorem ipsum",
+	"message" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit..."),
+array(
+	"author" => "Jens",
+	"title" => "Lorem ipsum dolor",
+	"message" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua...")
+);
