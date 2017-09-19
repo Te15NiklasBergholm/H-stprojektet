e9d6c3d3d5272443ae49632c83a57e032f1953da
jensnti <jens.andreasson@nti.se>
2017-09-19 10:24:43 +0200

    text length

--- data.php
+++ data.php
@@ -4,14 +4,28 @@ $data = array(
 array(
 	"author" => "Jens",
 	"title" => "Lorem",
-	"message" => "Lorem ipsum dolor sit amet..."),
+	"message" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
 array(
 	"author" => "Jens",
 	"title" => "Lorem ipsum",
-	"message" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit..."),
+	"message" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
 array(
 	"author" => "Jens",
 	"title" => "Lorem ipsum dolor",
 	"message" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua...")
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 );
